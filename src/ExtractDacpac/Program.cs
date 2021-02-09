c436b35f65f256862e495ff4b7c6cf13927a1369
Mac Plewa <Maciej.Plewa@velrada.com>
2021-02-09 12:27:16 +0800

    Implemented T-SQL script generation and DACPAC extract

---
+++ src/ExtractDacpac/Program.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Mac Plewa
+// Licensed under the MIT License
+
+using CommandLine;
+using DacFxCommon;
+
+namespace ExtractDacpac
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Parser.Default.ParseArguments<ExtractOptions>(args).WithParsed(Operations.RunExtract);
+		}
+	}
+}
---
+++ src/GenerateScript/Program.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Mac Plewa
+// Licensed under the MIT License
+
+using CommandLine;
+using DacFxCommon;
+
+namespace GenerateScript
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Parser.Default.ParseArguments<GenerateScriptOptions>(args).WithParsed(Operations.RunGenerateScript);
+		}
+	}
+}
